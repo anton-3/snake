93376bdba0a1f8f731b851277102306ca4f63305
anton-a-a <65707789+anton-a-a@users.noreply.github.com>
2021-03-28 14:04:44 -0500

    Update snake.js

--- snake.js
+++ snake.js
@@ -11,12 +11,30 @@ let paused = false;
 let score = 0;
 // length of snake that hasn't shown up on screen yet
 let pendingLength = 0;
-// array of all snake body cell divs
+// array of all snake body cells
 const snakeBody = [];
 const scoreElement = document.querySelector("#score");
 const highScoreElement = document.querySelector("#highscore");
+const alertBox = document.querySelector("#alert-box");
+const alertTitle = document.querySelector("#alert-box .alert-title");
+const alertDescription = document.querySelector(
+  "#alert-box .alert-description"
+);
 const snakeLengthPerApple = 5;
+// this stuff is to prevent 180 turns
+let directionAtLastMove;
+let queuedTurn;
+let gamesPlayed = 0;
 
+// set highscore from localStorage if it exists
+if (localStorage.getItem("highscore")) {
+  highScoreElement.textContent = localStorage.getItem("highscore");
+}
+
+// keyboard support
+document.onkeydown = keyControls;
+
+// assign keydown event codes to their directions
 const keys = {
   KeyA: "left",
   ArrowLeft: "left",
@@ -28,24 +46,26 @@ const keys = {
   ArrowDown: "down",
 };
 
-function playSnake(gameSpeed = 100, height = 35, width = 45) {
-  // gamespeed is how many milliseconds per game loop
-  // height and width for size of snake board
+function log(str) {
+  document.querySelector("#console").textContent += "\n" + str;
+}
 
+function playSnake(gameSpeed = 50, height = 35, width = 45) {
+  alertBox.style.display = "none";
+  if (gamesPlayed > 0) resetGame();
   createGrid(height, width);
-
-  document.onkeydown = keyControls;
-
   gameLoop = setInterval(gameLoopFunc, gameSpeed);
 }
 
 function gameLoopFunc() {
-  if (!direction || paused) return;
+  // if hasn't moved yet or the game is paused or alert box is showing
+  if (!direction || paused || alertBox.style.display !== "none") return;
 
   const oldCell = well[headRow][headCol];
   oldCell.className = "empty";
 
   updateSnakeLocation();
+  directionAtLastMove = direction;
 
   if (checkSnakeDead()) {
     endGame();
@@ -53,12 +73,7 @@ function gameLoopFunc() {
   }
 
   const newCell = well[headRow][headCol];
-  if (newCell.className === "apple") {
-    score += snakeLengthPerApple;
-    pendingLength += snakeLengthPerApple;
-    makeNewApple();
-    updateScoreboard();
-  }
+  if (newCell.className === "apple") eatApple();
   newCell.className = "snake-head";
 }
 
@@ -89,20 +104,62 @@ function updateSnakeLocation() {
       headRow++;
       break;
   }
+
+  if (queuedTurn) {
+    direction = queuedTurn;
+    queuedTurn = null;
+  }
+}
+
+function eatApple() {
+  score += snakeLengthPerApple;
+  pendingLength += snakeLengthPerApple;
+  makeNewApple();
+  updateScoreboard();
 }
 
 function updateScoreboard() {
   scoreElement.textContent = score.toString();
   if (score > parseInt(highScoreElement.textContent)) {
     highScoreElement.textContent = score.toString();
+    localStorage.setItem("highscore", score.toString());
   }
 }
 
 function keyControls(e) {
-  // if game is unpaused and the key pressed is a direction key
-  if (!paused && keys[e.code]) {
+  // if game is unpaused and the key pressed is a direction key and alert box is hidden
+  if (!paused && keys[e.code] && alertBox.style.display === "none") {
     e.preventDefault();
+    if (queuedTurn) return;
+    const code = keys[e.code];
+    if (
+      score !== 0 &&
+      ((direction === "up" && code === "down") ||
+        (direction === "right" && code === "left") ||
+        (direction === "down" && code === "up") ||
+        (direction === "left" && code === "right"))
+    ) {
+      // if snake has any body AND tries to turn opposite its current direction
+      return;
+    }
+    if (
+      score !== 0 &&
+      ((directionAtLastMove === "up" && code === "down") ||
+        (directionAtLastMove === "right" && code === "left") ||
+        (directionAtLastMove === "down" && code === "up") ||
+        (directionAtLastMove === "left" && code === "right"))
+    ) {
+      // if snake has any body AND its previous direction
+      // was opposite the direction it's trying to turn
+      queuedTurn = code;
+      return;
+    }
+
     direction = keys[e.code];
+  } else if (e.code === "Escape") {
+    playOrPause();
+  } else if (e.code === "Enter" && alertBox.style.display !== "none") {
+    playSnake();
   }
 }
 
@@ -161,18 +218,82 @@ function makeNewApple() {
 
 function checkSnakeDead() {
   if (
+    // hits a wall
     headRow < 0 ||
     headCol < 0 ||
     headRow >= well.length ||
-    headCol >= well[0].length
+    headCol >= well[0].length ||
+    // hits a snake body
+    snakeBody.includes(well[headRow][headCol])
   ) {
     return true;
   }
+  return false;
 }
 
 function endGame() {
   clearInterval(gameLoop);
-  alert("you died :(");
+  gamesPlayed++;
+  makeDeadHead();
+  alertTitle.textContent = "You died :(";
+  alertDescription.textContent = "Play again?";
+  setTimeout(() => {
+    alertBox.style.display = "";
+  }, 500);
+}
+
+function resetGame() {
+  direction = null;
+  if (paused) playOrPause();
+  headRow = 3;
+  headCol = 3;
+  score = 0;
+  updateScoreboard();
+  pendingLength = 0;
+  snakeBody.length = 0;
+  directionAtLastMove = null;
+  queuedTurn = null;
+  well.length = 0;
+  document.querySelector("#main").remove();
+}
+
+// make the snake head gray when it dies
+function makeDeadHead() {
+  if (well[headRow] && well[headRow][headCol]) {
+    // if snake head died inside the grid
+    well[headRow][headCol].className = "dead-snake-head";
+  } else {
+    // if snake died by hitting a wall
+    const deadHead = document.createElement("div");
+    deadHead.style.height = deadHead.style.width = "1em";
+    deadHead.style.position = "relative";
+    deadHead.className = "dead-snake-head";
+    // lastsquare is the last square the head was on before dying
+    let lastSquare;
+
+    switch (directionAtLastMove) {
+      case "left":
+        lastSquare = well[headRow][headCol + 1];
+        lastSquare.appendChild(deadHead);
+        deadHead.style.right = "1em";
+        break;
+      case "up":
+        lastSquare = well[headRow + 1][headCol];
+        lastSquare.appendChild(deadHead);
+        deadHead.style.bottom = "1em";
+        break;
+      case "right":
+        lastSquare = well[headRow][headCol - 1];
+        lastSquare.appendChild(deadHead);
+        deadHead.style.left = "1em";
+        break;
+      case "down":
+        lastSquare = well[headRow - 1][headCol];
+        lastSquare.appendChild(deadHead);
+        deadHead.style.top = "1em";
+        break;
+    }
+  }
 }
 
 function playOrPause() {
